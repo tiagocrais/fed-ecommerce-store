74f6ec264cf86eb1b70241e177aa1309dfdbb84e
Tiago Crais Camargo <tiagocrais@gmail.com>
2024-12-07 16:58:03 -0300

    Alterando arquivo de rota e instalando biblioteca do design system da Bulma.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
